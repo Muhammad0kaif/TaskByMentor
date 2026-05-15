74647d75be46b77977c44256d5faf60e49bfb8c9
Muhammad0kaif <111998659+Muhammad0kaif@users.noreply.github.com>
2026-05-15 15:16:02 +0530

    dont

--- RoleBased/WebApi/obj/Debug/net10.0/WebApi.AssemblyInfo.cs
+++ RoleBased/WebApi/obj/Debug/net10.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6afaa2f35e33dd68c14f1a909087dc216883fc75")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
